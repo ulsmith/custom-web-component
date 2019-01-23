8d808b8705337bb44ce4585949553aa32c4ea8bf
Paul Smith <p@ulsmith.net>
2019-01-23 16:16:50 +0000

    Added custom html button to extend a html button element via <button is=""></button> This is a templateless component so logic only, styles can be adjusted via standard JS style property. Be carefull altering styles of extended components. only set non overidable styles.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import CustomHTMLElement from './src/CustomHTMLElement.js'
+import CustomHTMLButtonElement from './src/CustomHTMLButtonElement.js'
 
 import { html } from '../lit-html/lit-html.js';
 
 export {
     CustomHTMLElement as CustomHTMLElement,
+    CustomHTMLButtonElement as CustomHTMLButtonElement,
     html as html
 }
