2b8ecda0b110a93d7d6f84dcc24597d1795dce99
Paul Smith <p@ulsmith.net>
2019-02-12 12:02:54 +0000

    removed button extending component as fails in IE11

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import CustomHTMLElement from './src/CustomHTMLElement.js'
-import CustomHTMLButtonElement from './src/CustomHTMLButtonElement.js'
 
 import { html } from '../lit-html/lit-html.js';
 
 export {
     CustomHTMLElement as CustomHTMLElement,
-    CustomHTMLButtonElement as CustomHTMLButtonElement,
     html as html
 }
